485de81658c0c2917c855c9cdb0454c9d8fa391c
RayhaanK <rayhaankhan331@gmail.com>
2023-06-13 14:51:25 +0200

    updated products

--- scripts/products.js
+++ scripts/products.js
@@ -20,8 +20,33 @@ Object.keys(product).forEach((item) => {
           <p class="card-text">
           "${newName.description}"
           </p>
+          <div class="modalBtn">
+          <button type="button" class="contentBtn" data-bs-toggle="modal" data-bs-target="#exampleModal` + newName.id +`">
+          Content
+          </button>
+          <div class="modal fade" id="exampleModal` + newName.id + `" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                ${newName.content}
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+              </div>
+              </div>
+          </div>
+          </div>
+          </div>
+          <div>
               <p class= "amount text-center">
+              <span class="boldW">
               R${newName.amount} 
+              </span>
               </p>
               <div class="addCart">
               <button class="cartBtn" onclick="addCheck(`+newName.id+`)">Add to Cart</button>
