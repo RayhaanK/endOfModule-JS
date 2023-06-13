da76ec5fe42329236847c6e6dffac132ba0e682b
RayhaanK <rayhaankhan331@gmail.com>
2023-06-13 09:41:50 +0200

    updated admin

--- scripts/admin.js
+++ scripts/admin.js
@@ -97,7 +97,33 @@ Object.keys(prod).forEach((item) => {
     <td><div class="tableRows">${newName.description}</td></div>
     <td><div class="tableRows">${newName.date}</td></div>
     <td><div class="tableRows">R${newName.amount}</td></div>
-    <td><div class="tableRows"><button>Edit</button><button>Delete</button></td></div>
+    <td><div class="tableRows"> <button type="button" class="addbtn mb-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Add Item
+  </button>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Add Item</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Title</p>
+          <input type="text" id="editTitle">
+          <p>Description</p>
+          <input type="text" id="editDescription">
+          <p>Image Link</p>
+          <input type="text" id="editImageLink">
+          <p>Price</p>
+          <input type="text" id="editPrice">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" id="updateProduct">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div><button>Delete</button></td></div>
     </tr>    
     `
 })
